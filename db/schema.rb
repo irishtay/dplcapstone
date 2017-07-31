b3830d15cb2cb667bd2d2124f9e82b43aaef1dbd
Taylor Hadean <taylorhadean@gmail.com>
2017-07-31 11:48:14 -0600

    add updated schema

---
+++ db/schema.rb
@@ -0,0 +1,129 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20170730184739) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "bios", force: :cascade do |t|
+    t.text "body"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "age"
+    t.string "gender"
+    t.string "name"
+    t.text "image_url"
+    t.string "state"
+    t.index ["user_id"], name: "index_bios_on_user_id"
+  end
+
+  create_table "friends", force: :cascade do |t|
+    t.integer "requesting_user_id"
+    t.string "requesting_user_name"
+    t.string "requesting_user_body"
+    t.string "requesting_user_img"
+    t.integer "receiving_user_id"
+    t.string "receiving_user_name"
+    t.string "receiving_user_body"
+    t.string "receiving_user_img"
+    t.boolean "accepted", default: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "comment"
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_messages_on_post_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "post_body"
+    t.string "state"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "sport_id"
+    t.integer "zip"
+    t.string "street_address"
+    t.string "city"
+    t.float "lat"
+    t.float "long"
+    t.index ["sport_id"], name: "index_posts_on_sport_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "sports", force: :cascade do |t|
+    t.string "name"
+    t.text "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.string "name"
+    t.string "nickname"
+    t.string "image"
+    t.string "email"
+    t.json "tokens"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
+  end
+
+  create_table "usersports", force: :cascade do |t|
+    t.string "skill_level"
+    t.bigint "sport_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["sport_id"], name: "index_usersports_on_sport_id"
+    t.index ["user_id"], name: "index_usersports_on_user_id"
+  end
+
+  add_foreign_key "bios", "users"
+  add_foreign_key "messages", "posts"
+  add_foreign_key "messages", "users"
+  add_foreign_key "posts", "sports"
+  add_foreign_key "posts", "users"
+  add_foreign_key "usersports", "sports"
+  add_foreign_key "usersports", "users"
+end
